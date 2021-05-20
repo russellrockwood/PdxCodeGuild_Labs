347d21c623536d0910afdc3739c5615b1587a445
Russell Rockwood <russell.rockwood@gmail.com>
2021-05-20 08:37:36 -0400

    transferred labs

---
+++ django/mysite/mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('contacts/', include('contacts.urls')),
+    path('pokedex/', include('pokedex.urls')),
+    
+]
